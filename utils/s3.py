b2853816c5b7810f8cc6d1e79a74b05ae117374e
Kritiman2005 <kingzkritiman@gmail.com>
2026-01-13 21:17:51 +0530

    fixed s3

--- utils/s3.py
+++ utils/s3.py
@@ -1,10 +1,25 @@
-import boto3
 import os
 
-s3 = boto3.client("s3")
-BUCKET = os.environ["S3_BUCKET"]
+try:
+    import boto3
+    S3_ENABLED = "S3_BUCKET" in os.environ
+except ImportError:
+    boto3 = None
+    S3_ENABLED = False
+
+BUCKET = os.environ.get("S3_BUCKET")
+
+if S3_ENABLED:
+    s3 = boto3.client("s3")
+else:
+    s3 = None
+
 
 def upload_dir(local_dir, s3_prefix):
+    if not S3_ENABLED:
+        print("⚠️ S3 upload skipped (S3_BUCKET not set)")
+        return
+
     for root, _, files in os.walk(local_dir):
         for file in files:
             local_path = os.path.join(root, file)
@@ -14,7 +29,12 @@ def upload_dir(local_dir, s3_prefix):
             )
             s3.upload_file(local_path, BUCKET, s3_key)
 
+
 def download_dir(s3_prefix, local_dir):
+    if not S3_ENABLED:
+        print("⚠️ S3 download skipped (S3_BUCKET not set)")
+        return
+
     paginator = s3.get_paginator("list_objects_v2")
     for page in paginator.paginate(Bucket=BUCKET, Prefix=s3_prefix):
         for obj in page.get("Contents", []):
